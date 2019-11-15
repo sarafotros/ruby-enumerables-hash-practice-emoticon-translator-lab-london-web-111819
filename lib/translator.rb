9dcfa70bb5a728d89796d302677c91273c013bb7
Sara Fotros <sarafotros1@yahoo.com>
2019-11-15 18:28:41 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,20 @@
 # require modules here
 
+require "yaml"
+emoticon = YAML.load_file ('emoticon.rb')
+
 def load_library
   # code goes here
+  load_library = YAML.load_file('emoticon.rb')
+  load_library
 end
 
 def get_japanese_emoticon
-  # code goes here
+  hash = {"get_japanese_emoticon"  = {} }
+   hash
 end
 
 def get_english_meaning
-  # code goes here
+  hash_ = { "get_english_meaning" = {} }
+  hash
 end
